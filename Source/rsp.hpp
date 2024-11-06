6d9f82a28f8db225dd82adff3c30bb9575d21b4c
eUltrabyte <goszymon1337@gmail.com>
2024-11-06 19:25:12 +0100

    Fix for windows

--- Source/rsp.hpp
+++ Source/rsp.hpp
@@ -4,16 +4,22 @@
 
 #if defined(_WIN32)
     #include <winsock2.h>
+    #include <io.h>
     #pragma comment(lib, "ws2_32.lib")
+
     #define RSP_WINDOWS
 #elif defined(__linux__)
     #include <sys/socket.h>
     #include <arpa/inet.h>
     #include <unistd.h>
+
     #define RSP_LINUX
 #else
     #error "rsp >> error - unknown operating system"
 #endif
+
+#include <stdlib.h>
+#include <stdio.h>
 
 #include <iostream>
 #include <string>
